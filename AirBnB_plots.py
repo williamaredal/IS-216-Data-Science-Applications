87f54ba25135be38129d599c53868759e5c11d35
williamaredal <williamaredal@hotmail.com>
2023-11-11 14:52:12 +0100

    number_of_reviews was already included

--- AirBnB_plots.py
+++ AirBnB_plots.py
@@ -47,7 +47,7 @@ columns = [
     'number_of_reviews', 'review_scores_rating', 'review_scores_accuracy',
     'review_scores_cleanliness', 'review_scores_checkin',
     'review_scores_communication', 'review_scores_location',
-    'review_scores_value', # 'reviews_per_month', 'number_of_reviews',
+    'review_scores_value', # 'reviews_per_month', 
     #'calculated_host_listings_count',
     #'calculated_host_listings_count_entire_homes',
     #'calculated_host_listings_count_private_rooms',
