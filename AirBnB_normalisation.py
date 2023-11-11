da6380752b7e8f2425973c41e08995cbf2fb43f1
williamaredal <williamaredal@hotmail.com>
2023-11-11 15:53:30 +0100

    found distance to city center

--- AirBnB_normalisation.py
+++ AirBnB_normalisation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 import re
 from sklearn.preprocessing import MinMaxScaler
 
@@ -8,7 +9,7 @@ def extract_number(text):
         return 0.0
     
     # if the string contains integer(s), return these
-    match = re.search(r'\d+', text)
+    match = re.search(r'\d+(\.\d+)?', text)
     if match:
         extractedInteger = float(match.group())
         return extractedInteger
@@ -18,6 +19,32 @@ def extract_number(text):
         return 1.0
 
 
+def find_distance_to_city_center(latitude, longitude):
+    # Latitude and longtitude distance to oslo 
+    oslo_lat_long = [59.911491, 10.757933] # from this source: https://www.latlong.net/place/oslo-ostlandet-norway-14195.html
+    distance_to_city_center = (abs(latitude - oslo_lat_long[0]) ** 2) + (abs(longitude - oslo_lat_long[1]) ** 2) ** 0.5
+
+    return distance_to_city_center
+
+
+def haversine_distance_to_city_center(latitude, lonitude):
+    # Hardcoded latitude and longitude example
+    # Latitude and longtitude distance to oslo 
+    oslo_lat, oslo_long = 59.911491, 10.757933 # from this source: https://www.latlong.net/place/oslo-ostlandet-norway-14195.html
+
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, oslo_lat, oslo_long = map(math.radians, [lat1, lon1, oslo_lat, oslo_long])
+
+    # Haversine formula
+    dlat = oslo_lat - lat1
+    dlon = oslo_long - lon1
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(oslo_lat) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a))
+    r = 6371  # Radius of Earth in kilometers
+
+    return c * r  # Distance in kilometers
+
+
 
 filename = 'listings-clean.csv'
 dataFrame = pd.read_csv(filename, encoding='latin-1')
@@ -25,18 +52,33 @@ dataFrame['number_bathrooms'] = dataFrame['bathrooms_text'].apply(extract_number
 
 columns = [
     'price', 
-    'latitude', 'longitude', 
-    'accommodates', 'bedrooms', 'beds', 'number_bathrooms',
-    'minimum_nights', 'maximum_nights',
-    'number_of_reviews', 'review_scores_rating', 'review_scores_accuracy',
-    'review_scores_cleanliness', 'review_scores_checkin',
-    'review_scores_communication', 'review_scores_location',
-    'review_scores_value', # 'reviews_per_month', 
-    #'calculated_host_listings_count',
-    #'calculated_host_listings_count_entire_homes',
-    #'calculated_host_listings_count_private_rooms',
-    #'calculated_host_listings_count_shared_rooms'
+    'latitude', 'longitude', 'distance_to_city_center',
+    'bedrooms', 'beds', 'number_bathrooms',
+    # 'kitchen_availability', 'place_for_yourself', 'host_is_superhost_numerical', 'amenities_count',  
+    'review_scores_rating', 'number_of_reviews', # host_is_superhost_numerical
+    # additional review types
+    #'review_scores_accuracy', 'review_scores_cleanliness', 'review_scores_checkin', 'review_scores_communication', 'review_scores_location', 'review_scores_value',
+
+    # additional columns than the ones we have in the google sheets document
+    #'accommodates', 'reviews_per_month', 
+     
 ]
+
+# Make the colums required for the model that are missing
+# Closest to city center,  Number of Baths (is this covered by number_bathrooms?), Kitchen Availability, 
+# Amenities (is this covered by amenities_count?), host_is_superhost 
+
+# Latitude and longtitude distance to oslo 
+dataFrame['distance_to_city_center'] = dataFrame.apply(lambda row: find_distance_to_city_center(row['latitude'], row['longitude']))
+
+# Numerical representation of true/false if the listing amenities contains "Kitchen"
+kitchen_availability = 0
+# Numerical representation of true/false if the listing room type is "place for yourself"
+place_for_yourself = 0
+# Converts superhost boolean values to numerical representation
+dataFrame['host_is_superhost_numerical'] = dataFrame['host_is_superhost'].apply(lambda row: 1 if row == 't' else 0)
+
+
 
 # TODO look at the column edges to find strange data to remove
 nan_counts = dataFrame[columns].isnull().sum()
@@ -50,23 +92,7 @@ for col in columns:
     print(f"column max value {max(dataFrame[col])}")
     print()
 
-
-# Make the colums required for the model that are missing
-# Closest to city center,  Number of Baths (is this covered by number_bathrooms?), Kitchen Availability, 
-# Amenities (is this covered by amenities_count?), host_is_superhost 
-
-# Latitude and longtitude distance to oslo 
-oslo_lat_long = [59.911491, 10.757933]
-closest_to_city_center = 0
-# Numerical representation of true/false if the listing amenities contains "Kitchen"
-kitchen_availability = 0
-# Numerical representation of true/false if the listing room type is not a "shared room" (should this include private room?)
-not_shared_room = 0
-# Numerical representation of true/false if the listing room type is "place for yourself"
-place_for_yourself = 0
-# Converts superhost boolean values to numerical representation
-dataFrame['host_is_superhost_numerical'] = dataFrame['host_is_superhost'].apply(lambda row: 1 if row == 't' else 0)
-
+exit()
 
 
 # Assuming df is your DataFrame
@@ -74,6 +100,7 @@ df_normalized = dataFrame.copy()
 
 
 # Find a mask for the NaN value rows/cells to ignore during normalisation
+# TODO the closest_to_city_center should be inverted
 scaler = MinMaxScaler()
 df_normalized[columns] = scaler.fit_transform(dataFrame[columns])
 print(df_normalized[columns].head())
